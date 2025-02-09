a11cb4a68771dfec266cfc691842d3b7b048a0b8
Mohsin Ali Rashid <mohsinalikhawaja1111@gmail.com>
2025-02-09 13:19:44 +0100

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:expense_manager/main.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+// Generate a mock class for LocalStorage
+@GenerateMocks([LocalStorage])
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Use mock LocalStorage
+    final mockLocalStorage = MockLocalStorage();
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(localStorage: mockLocalStorage));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -28,3 +28,5 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+class MockLocalStorage extends Mock implements LocalStorage {}
